c3d1c5b8b478005506f2de7a3c062700428d621a
Thomas Noriega <trn@trn.local>
2018-01-18 14:20:26 -0800

    Add potentially dynamic images, links and labels

--- classifier/views.py
+++ classifier/views.py
@@ -7,9 +7,17 @@ def index(request):
 
 def results(request):
     context_dict = {
-        'label1': 'Top result',
-        'label2': 'Second result',
-        'label3': 'third result',
+        'result1_path': 'results/Acura-Integra_Type_R-2001_0.jpg',
+        'result1_label': 'Top result',
+        'result1_link': 'https://www.cars.com',
+
+        'result2_path': 'results/Acura-Integra_Type_R-2001_1.jpg',
+        'result2_label': 'Second result',
+        'result2_link': 'https://www.cars.com',
+
+        'result3_path': 'results/Acura-Integra_Type_R-2001_3.jpg',
+        'result3_label': 'Third result',
+        'result3_link': 'https://www.cars.com',
         }
     return render(request, 'classifier/results.html', context=context_dict)
 
