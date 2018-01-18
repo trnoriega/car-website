3d7a3f935963b890c836eff48b0d1a8a7a539b48
Thomas Noriega <trn@trn.local>
2018-01-17 16:55:51 -0800

    First stab at classifier models

--- classifier/models.py
+++ classifier/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    label = models.CharField(max_length=200)
+    make = model.CharField(max_length=100)
+    model = model.CharField(max_length=150)
+    year = model.CharField(max_length=4)
+
+class InputImage(models.Model):
+    # file will be saved to MEDIA_ROOT/uploads
+    upload = models.ImageField(upload_to='uploads/')
+
