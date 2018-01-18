e5672db27ca6976e01fd56adf0bfe632dc3d8501
Thomas Noriega <trn@trn.local>
2018-01-18 09:59:30 -0800

    Add upload icon and make it a link

--- classifier/views.py
+++ classifier/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Classifying cars is awesome!'}
+    context_dict = {}
     return render(request, 'classifier/index.html', context=context_dict)
 
 def results(request):
