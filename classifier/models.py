d1ee6f6125f88299c4b0f6cc972148da7ccf843a
Thomas Noriega <trn@trn.local>
2018-01-17 20:38:58 -0800

    Tweak model to include file paths

--- classifier/models.py
+++ classifier/models.py
@@ -1,10 +1,16 @@
+import os
+
+from django.conf import settings
 from django.db import models
 
 class Car(models.Model):
     label = models.CharField(max_length=200)
-    make = model.CharField(max_length=100)
-    model = model.CharField(max_length=150)
-    year = model.CharField(max_length=4)
+    make = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=150)
+    year = models.CharField(max_length=4)
+    path = FilePathField(path=os.path.join(settings.MEDIA_DIR, 'results'))
+    def __str__(self):
+        return self.label
 
 class InputImage(models.Model):
     # file will be saved to MEDIA_ROOT/uploads
