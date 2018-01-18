95585d65a56eacfea1dc56d7d48e3d7a33977d38
Thomas Noriega <trn@trn.local>
2018-01-18 08:57:51 -0800

    Add results and about page urls and views

--- classifier/urls.py
+++ classifier/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',  views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'results', views.results, name='results'),
+    url(r'about', views.about, name='about')
 ]
--- classifier/views.py
+++ classifier/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': 'Classifying cars is awesome!'}
     return render(request, 'classifier/index.html', context=context_dict)
+
+def results(request):
+    return HttpResponse('This is the results page', )
+
+def about(request):
+    return HttpResponse('This is the About page')
