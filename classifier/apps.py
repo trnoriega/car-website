c3bb5ccbf171183a5cdee8825d7de07656d587b4
Thomas Noriega <trnoriega@users.noreply.github.com>
2018-01-25 12:49:25 -0800

    Working implementation of the consensus body results considering top percent and top-5!

--- classifier/apps.py
+++ classifier/apps.py
@@ -1,8 +1,49 @@
 from django.apps import AppConfig
-from keras import backend as K
 
-# from .algorithms import load_model, load_lookup_dicto # for production
-from .fast_algorithms import load_model, load_lookup_dicto # for fast testing
+import os
+import pickle
+
+from keras import backend as K
+from keras.models import model_from_json
+
+BASE_DIR = os.path.join(
+    os.path.expanduser('~'), 'Dropbox', 'projects',
+    'cars', 'data', 'InceptionV3'
+    )
+
+WEIGHT_PATH = os.path.join(
+    BASE_DIR,
+    'InceptionV3_21_FT3_30_40.h5'
+    )
+
+JSON_PATH = os.path.join(
+    BASE_DIR,
+    'InceptionV3_21_FT3_30_40.json'
+    )
+
+LOOKUP_PATH = os.path.join(
+    BASE_DIR,
+    'I_15_lookup_dict.pkl'
+    )
+
+def load_model():
+    """
+    Initializes and loads pretrained weights into keras model to be used for predictions
+    """
+    with open(JSON_PATH, 'r') as json_file:
+        loaded_model_json = json_file.read()
+    json_model = model_from_json(loaded_model_json)
+    json_model.load_weights(WEIGHT_PATH)
+
+    return json_model
+
+def load_lookup_dicto():
+    """
+    returns lookup_dictonary necessary to interpret keras model predictions
+    """
+    with open(LOOKUP_PATH, 'rb') as f:
+        lookup_dicto = pickle.load(f)
+    return lookup_dicto
 
 class ClassifierConfig(AppConfig):
     name = 'classifier'
