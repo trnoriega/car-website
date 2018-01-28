e7a3b3ec990124077e89ee26bda8b19cca0f6719
Thomas Noriega <trnoriega@users.noreply.github.com>
2018-01-28 14:18:33 -0800

    Testing how to run collectstatic

--- cars/settings.py
+++ cars/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_ROOT = STATIC_DIR
+STATICFILES_DIRS = []
 
 # Media files
 
